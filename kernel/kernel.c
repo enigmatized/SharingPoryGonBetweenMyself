08e4f6d74f1bdeec4b1f6a13aa21bc87d2bde749
Think <going.to.any.lengths.work@gmail.com>
2019-11-11 08:18:21 -0800

    New upload

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -23,36 +23,45 @@ void kernel_main() {
 	paging_init();
 	_prints("Post post paging");
 	interrupts_init();
+
+	paging_init();
+	_prints("Post post paging\n");
+	
+	
+	void keyboard_init();
+	
+	_prints("\n Are we getting past KeyBoard Init?");
+	_prints("\n Pre Interrupts_init");
+	interrupts_init();
+	_prints("\n Post Interrupts_init");
     // isr_install();
     // irq_install();
 
-    // asm("int $2");
-    // asm("int $3");
+     asm("int $2");
+     asm("int $3");
 
-    // _cscreen();
-    // _prints("Type something, it will go through the kernel\n"
-    //     "Type END to halt the CPU or PAGE to request a kmalloc()\n> ");
+     //_cscreen();
+     _prints("\nType something, it will go through the kernel\n");}
+
+void user_input(char *input) {
+    if (strcmp(input, "END") == 0) {
+        _prints("Stopping the CPU. Bye!\n");
+        asm volatile("hlt");
+    } else if (strcmp(input, "PAGE") == 0) {
+        /* Lesson 22: Code to test kmalloc, the rest is unchanged */
+        uint32_t phys_addr;
+        uint32_t page = kmalloc(1000, 1, &phys_addr);
+        char page_str[16] = "";
+        hex_to_ascii(page, page_str);
+        char phys_str[16] = "";
+        hex_to_ascii(phys_addr, phys_str);
+        _prints("Page: ");
+        _prints(page_str);
+        _prints(", physical address: ");
+        _prints(phys_str);
+        _prints("\n");
+    }
+    _prints("You said: ");
+    _prints(input);
+    _prints("\n> ");
 }
-
-// void user_input(char *input) {
-//     if (strcmp(input, "END") == 0) {
-//         _prints("Stopping the CPU. Bye!\n");
-//         asm volatile("hlt");
-//     } else if (strcmp(input, "PAGE") == 0) {
-//         /* Lesson 22: Code to test kmalloc, the rest is unchanged */
-//         uint32_t phys_addr;
-//         uint32_t page = kmalloc(1000, 1, &phys_addr);
-//         char page_str[16] = "";
-//         hex_to_ascii(page, page_str);
-//         char phys_str[16] = "";
-//         hex_to_ascii(phys_addr, phys_str);
-//         _prints("Page: ");
-//         _prints(page_str);
-//         _prints(", physical address: ");
-//         _prints(phys_str);
-//         _prints("\n");
-//     }
-//     _prints("You said: ");
-//     _prints(input);
-//     _prints("\n> ");
-// }
